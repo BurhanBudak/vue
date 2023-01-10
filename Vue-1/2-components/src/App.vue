637f80d53c9866371bdabe0d415f28414e112adf
BurhanBudak <budak.burha@gmail.com>
2023-01-10 23:45:07 +0100

    vue-1

---
+++ Vue-1/2-components/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <appNavbar/>
+    <Hero/>
+    <testimonials/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/navigation/Navbar.vue';
+
+import  Hero  from "./components/Hero/Hero.vue";
+import Testimonials from './components/Testimonials/Testimonials.vue';
+export default {
+  name: 'App',
+  components: {
+    appNavbar: Navbar,
+    Hero,
+    Testimonials
+  }
+}
+</script>
+
+<style>
+
+</style>
