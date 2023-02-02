bf628af7e10b68a90d0d1590a9417a750943b9d3
BurhanBudak <budak.burha@gmail.com>
2023-02-02 22:10:19 +0100

    vue-2

---
+++ Vue-2/1-functions/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from '../../../Vue-1/2-components/src/components/navigation/Navbar.vue'
+export default {
+  name: 'App',
+  components: { Navbar },
+
+}
+</script>
+
+<style>
+
+</style>
